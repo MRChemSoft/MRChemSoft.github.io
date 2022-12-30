c93890046f7d43a775cf3e5cf3f9c36e4b106e97
Roberto Di Remigio Eikås <roberto@totaltrash.xyz>
2022-12-30 13:25:20 +0100

    chore: clean up `flake.nix`

--- flake.nix
+++ flake.nix
@@ -5,17 +5,19 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [
-	    quarto
-	    zola
-          ];
-        };
-      });
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          dig
+          quarto
+        ];
+      };
+    });
 }
